e1e8e45e41ce9257cbb2a997900beace5ebfd9d7
100101101 <96186091+100101101@users.noreply.github.com>
2024-02-26 01:24:20 +0800

    Initial commit for Streamlit app

---
+++ app.py
@@ -0,0 +1,7 @@
+# Streamlit Application for Diabetes Prediction
+
+import streamlit as st
+
+st.title('Diabetes Prediction Application')
+
+st.write('This is a simple Streamlit application that uses a machine learning model to predict diabetes.')
